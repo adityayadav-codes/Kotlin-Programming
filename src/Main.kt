c2d0163b58dbc78bbe362c1685eee989126c45f0
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-13 17:03:54 +0530

    start Kotlin Programming

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package org.example
+
+fun main() {
+//    print("ldksajf;alksdf")
+//    println("Hello, \n\n\n\nworld!!!")
+
+
+
+//    println("Hello world!!")
+
+//    println(19)
+//    println(9.8)
+//    println("My age is "+19) // valid statement
+//    println("My age is ${19 + 1}") // string interpolation (template string)
+    println("\n\nMy name is Aadi yaduvanshiii")
+
+    var a = 10
+    var b = 20
+    println("Sum ${a+b}")
+}
