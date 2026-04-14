db36fc2294670810c3e5f0e58f8f1122e643c33f
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-14 15:05:58 +0530

    Give an assesment

--- src/Main.kt
+++ src/Main.kt
@@ -8,7 +8,7 @@ fun main() {
 
     print("hello kotlin ")
     val name = "Aditya yadav"
-    var age = 20;
+    var age = 20
 
     println("Name : $name")
     println("Age : $age")
@@ -20,10 +20,35 @@ fun main() {
     val d: Boolean = true
     val e: String = "How r u"
 
-    println("Integer : "+a)
-    println("Double  : "+b)
+    println("Integer : ${a}")
+    println("Double  : ${b}")
     println(c +" Char <---")
-    println("Char    : "+c)
-    println("Boolean : "+d)
-    println("String  : "+e)
+    println("Char    : ${c}")
+    println("Boolean : ${d}")
+    println("String  : ${e}")
+
+    // Input in kotlin------------------------
+    print("Enter location : ")
+    val location = readlnOrNull()
+    println("Location is $location")
+
+    // Assesment-----------
+    /*
+        1.	Print your name, age, and city
+        2.	Take input and print greeting
+        3.	Create 5 variables using val and var
+        4.	Try string templates
+        5.	Convert Java variable code → Kotlin
+    */
+//  1.	Print your name, age, and city
+    val StarName = "Virat Kohli"
+    var StarAge: Int = 20
+    var city: String = "Uttar Pradesh"
+
+    println("Star Name : ${StarName}")
+    println("Star Age  : ${StarAge}")
+    println("City      : ${city}")
+
+//   2.	Take input and print greeting
+
 }
