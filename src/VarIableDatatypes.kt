8f3571fae6f584216565e8b1a350c3562db856ac
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-15 22:07:48 +0530

    variables & datatypes

---
+++ src/VarIableDatatypes.kt
@@ -0,0 +1,44 @@
+package org.example
+import java.util.Scanner;
+fun main() {
+    val input = (System.`in`)
+//    var  a = 10
+//    var b = 20
+//    println(a)
+//    println(b)
+//    val string = "My name is Aadi"
+//    println(string.contains("name"))
+//    println("Aadi" in string)
+//    println("aadi" in string)
+//    println("aadi" !in string)
+
+
+    // By using readl()
+//    print("Enter your name : ")
+//    val name = readln()
+//
+//    print("Enter your age : ")
+//    val age = readln().toInt()
+//
+//    print("Enter your Gender : ")
+//    val gender = readln()[0]
+//
+//    print("Enter your 10th cgpa : ")
+//    val cgpa = readln().toFloat()
+//
+//    println("your name is $name")
+//    println("Your age is $age")
+//    println("Your Gender is $gender")
+//    println("Your cgpa is $cgpa")
+    //
+    // 1. Write a C program to perform input/output of all basic data types
+    print("Enter the value of a : ")
+    val a = readln()
+
+    print("Enter the value of b : ")
+    val b = readln()
+
+    print("a : $a\t")
+    print("b : $b")
+
+}
