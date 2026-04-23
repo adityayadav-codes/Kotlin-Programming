ec3daff028130cfd1e2edccebd31c03c6730ef68
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-23 14:02:14 +0530

    Loops and Range in Kotlin

---
+++ src/Loops and Range.kt
@@ -0,0 +1,49 @@
+package org.example
+
+// Print numbers from 1 to 100
+
+
+//fun main() {
+//
+//    for (i in 1..100) {
+//        print("$i \t")
+//    }
+//}
+
+// print even numbers from 1 to 50
+
+//fun main() {
+//
+//    for(i in 2..50 step 2) {
+//
+//        print("$i \t")
+//    }
+//}
+
+
+// print number in reverse 50 to 1
+
+//fun main() {
+//
+//    for(i in 50 downTo 1) {
+//
+//        print("$i \t")
+//    }
+//}
+
+
+// find the sum of number from n
+
+//fun main() {
+//
+//    var sum = 0;
+//
+//    print("Enter the n or limit : ")
+//    val n = readln().toInt()
+//
+//    for( i in 1..n) {
+//
+//        sum = sum + i
+//    }
+//    print("Sum : $sum")
+//}
