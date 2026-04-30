789167ac10dde254e4fec163d81f1150987978dc
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-30 14:26:42 +0530

    to replace oddd by cube

---
+++ replaceAllOddByCUBE.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    val list = Array(5) {0}
+
+    println("Enter the elements : ")
+
+    for (i in 0 until list.size) {
+
+        list[i] = readln().toInt()
+    }
+
+    for(i in 0 until list.size) {
+
+        if(list[i] % 2 != 0) {
+
+            list[i] = list[i] * list[i] * list[i]
+        }
+    }
+    println(list.joinToString())
+}
