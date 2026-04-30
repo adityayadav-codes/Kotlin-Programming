8115e947d3666aedbde7c88a2884c78faeb6e93f
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-30 14:38:37 +0530

    ToReplaceEvenByZeroOddByOne

---
+++ ToReplaceEvenByZeroOddByOne.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    val list = Array(5){0}
+
+    println("Enter the elements : ")
+    for (i in 0 until list.size) {
+
+        list[i] = readln().toInt()
+    }
+
+    for (i in 0 until list.size) {
+
+        if(list[i] %2 == 0) {
+
+            list[i] = 0
+        }
+        else {
+
+            list[i] = 1
+        }
+    }
+    println(list.joinToString())
+}
